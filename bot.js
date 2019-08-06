2ce1e072be2d6feb47914778c3eae421a9c9d776
killua1001 <53825064+killua1001@users.noreply.github.com>
2019-08-06 16:40:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("اكتب اي شي",{type: 'WATCHING'})
+   client.user.setActivity("ا+play",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -37,7 +37,7 @@ const queue = new Map();
  
  
  
-var prefix = "البرفكس" 
+var prefix = "+" 
 client.on('message', async msg => {
     if (msg.author.bot) return undefined;
    
